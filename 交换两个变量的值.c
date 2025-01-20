3cf1993a96e3612878d20dceaa3705ca3377c10f
2084138876 <2084138876@qq.com>
2025-01-20 16:27:41 +0800

    Add files via upload

---
+++ 交换两个变量的值.c
@@ -0,0 +1,30 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//
+//
+////交换函数
+//int sw(int* px, int* py)
+//{
+//	int* z = 0;
+//	z =* px;//px是a的地址
+//	*px = *py;//py是b的地址
+//	*py = z;
+//}
+//
+//int main()
+//{
+//	//当实参传递给形参的时候，形参是实参的一份临时拷贝
+//	//对形参的修改不会影响实参
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("交换前，a=%d b=%d\n", a, b);
+//	sw(&a,&b);
+//	printf("交换后，a=%d b=%d\n", a, b);
+//
+//
+//
+//
+//	return 0;
+//}
